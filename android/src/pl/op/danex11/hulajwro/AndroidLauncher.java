f605bbbaea748357cbcf01496fbadb854539828b
danex11 <danex11@op.pl>
2021-10-12 15:28:51 +0200

    +adMob configured, added bannerView but not functional yet

--- android/src/pl/op/danex11/hulajwro/AndroidLauncher.java
+++ android/src/pl/op/danex11/hulajwro/AndroidLauncher.java
@@ -3,18 +3,70 @@ package pl.op.danex11.hulajwro;
 import android.annotation.TargetApi;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 //import pl.op.danex11.hulajwro.SpielFahre;
 
 public class AndroidLauncher extends AndroidApplication {
+
+
+    private AdView adView;
+
+                                           
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-        initialize(new SpielFahre(), config);
+
+        //initialize(new SpielFahre(), config);
+        //View gameView=initializeForView(new SpielFahre(), config);
+        //AdMob
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        //AdMob
+        RelativeLayout layout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        layout.setLayoutParams(params);
+
+        ////View gameView=initializeForView(new BuckyRun(), config);
+        View gameView=initializeForView(new SpielFahre(), config);
+
+        RelativeLayout.LayoutParams gameViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        gameViewParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+        gameViewParams.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+
+        gameView.setLayoutParams(gameViewParams);
+        layout.addView(gameView);
+
+        adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        // ca-app-pub-3940256099942544/6300978111 == test Id for loading testing ads
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+
+        AdRequest.Builder adRequestBuilder = new AdRequest.Builder();
+        adView.loadAd(adRequestBuilder.build());
+
+        RelativeLayout.LayoutParams topParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        topParams.addRule(RelativeLayout.ALIGN_PARENT_TOP,RelativeLayout.TRUE);
+        topParams.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+        layout.addView(adView, topParams);
+        adView.setBackgroundColor(android.graphics.Color.TRANSPARENT);
+
+
     }
 
     //hide software home, back, recent buttons
