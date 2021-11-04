27e9b604f8d77f7e97f833416aa25b834ed998d7
danex11 <danex11@op.pl>
2021-11-04 08:41:26 +0100

    +adMob test banner functional - shows only with internet connection avaliable

--- android/src/pl/op/danex11/hulajwro/AndroidLauncher.java
+++ android/src/pl/op/danex11/hulajwro/AndroidLauncher.java
@@ -1,10 +1,15 @@
 package pl.op.danex11.hulajwro;
 
 import android.annotation.TargetApi;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.google.android.gms.ads.AdRequest;
@@ -14,18 +19,21 @@ import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
+import static java.lang.Boolean.TRUE;
+
 //import pl.op.danex11.hulajwro.SpielFahre;
 
 public class AndroidLauncher extends AndroidApplication {
 
 
-    private AdView adView;
+    //private AdView adView;
 
                                            
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 
         //initialize(new SpielFahre(), config);
@@ -37,35 +45,67 @@ public class AndroidLauncher extends AndroidApplication {
             }
         });
 
-        //AdMob
+        //Layout params
         RelativeLayout layout = new RelativeLayout(this);
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
         layout.setLayoutParams(params);
 
-        ////View gameView=initializeForView(new BuckyRun(), config);
-        View gameView=initializeForView(new SpielFahre(), config);
+        //set general view to @layout
+        setContentView(layout);
+        Log.d("tagA", "Layout Loaded once0");
 
+
+
+        ////View gameView=initializeForView(new BuckyRun(), config);
+        //initialize gameview
+        View gameView=initializeForView(new SpielFahre(), config);
+        //parameters of gameview
         RelativeLayout.LayoutParams gameViewParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
         gameViewParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
         gameViewParams.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
-
         gameView.setLayoutParams(gameViewParams);
+        //add gameview to @layout
         layout.addView(gameView);
-
-        adView = new AdView(this);
+        //ad1: add adview
+        AdView adView = new AdView(this);
         adView.setAdSize(AdSize.BANNER);
-        // ca-app-pub-3940256099942544/6300978111 == test Id for loading testing ads
+        TextView textview = new TextView(this);
+        textview.setText("this textview stays behind an Ad");
+        textview.setTextSize(10);
+        //ca-app-pub-3940256099942544/6300978111 == test Id for loading testing ads
         adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
-
-        AdRequest.Builder adRequestBuilder = new AdRequest.Builder();
-        adView.loadAd(adRequestBuilder.build());
-
-        RelativeLayout.LayoutParams topParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        topParams.addRule(RelativeLayout.ALIGN_PARENT_TOP,RelativeLayout.TRUE);
+        //add adview to @layout
+        RelativeLayout.LayoutParams topParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        topParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,RelativeLayout.TRUE);
         topParams.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+        layout.addView(textview,topParams);
+        //adView.setBackgroundColor(android.graphics.Color.TRANSPARENT);
+        //adView.setBackgroundColor(Color.RED);
         layout.addView(adView, topParams);
-        adView.setBackgroundColor(android.graphics.Color.TRANSPARENT);
 
+
+
+        //ad2: initialize and parametrize adview
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        //request and load ad
+        //AdRequest.Builder adRequestBuilder = new AdRequest.Builder();
+        //adView.loadAd(adRequestBuilder.build());
+        //ad3 : request and load
+        //Create request.
+        AdRequest adRequest = new AdRequest.Builder().
+                build();
+        //adRequest.setTesting(true);
+        //load an ad <<< Loads only while connection to Internet is avaliable
+        adView.loadAd(adRequest);
+
+
+        //set general view to @layout
+        setContentView(layout);
 
     }
 
